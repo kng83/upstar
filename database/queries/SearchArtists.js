d629e0827d5ddc7e5d94a8b0b84fe716a1df413c
unknown <pawel.kng@gmail.com>
2018-01-05 14:24:35 +0100

    cc

--- database/queries/SearchArtists.js
+++ database/queries/SearchArtists.js
@@ -8,10 +8,45 @@ const Artist = require('../models/artist');
  * @param {integer} limit How many records to return in the result set
  * @return {promise} A promise that resolves with the artists, count, offset, and limit
  * like this: {all:[artists], count:count, offset:offset, limit: limit}
+ * Write a query that will sort, offset, limit options only
+ * don't worry about criteria yet
+ * sortby jest na dole 2.14 (czyli sort by age, name lub album released
  */
 module.exports = (criteria, sortProperty, offset = 0, limit = 20) => {
-    //Write a query that will sort, offset, limit options only
-    //don't worry about criteria yet
-    //sortby jest na dole 2.14 (czyli sort by age, name lub album released
-    console.log(sortProperty);
+
+    let sortObj= {name:1};
+    let count =5;
+
+    switch(sortProperty){
+        case 'name':{
+            sortObj = {name:1};
+        }
+        break;
+        case 'age':{
+            sortObj ={age:1};
+        }
+        break;
+        case 'albums':{
+            sortObj = {albums:-1};
+
+        }
+        break;
+        default:
+            break;
+    }
+
+return new Promise((resolve) => {
+     Artist.find({}).sort(sortObj).then((artist)=>{
+
+        let output=[];
+        for (let i=0; i<artist.length;i++){
+            output.push(artist[i]._doc);
+        }
+        return output;
+
+    }).then((artistSort)=>{
+        resolve({all:artistSort,count:artistSort.length,offset:offset,limit:limit});
+    });
+});
+
 };
